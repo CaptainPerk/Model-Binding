0fe38ddaea5e469b4160c29f3622093a02c05a27
Jon Perkins <captainperk81@gmail.com>
2018-11-17 19:53:23 -0500

    Selectivly Binding Properties

--- MvcModels/Models/AddressSummary.cs
+++ MvcModels/Models/AddressSummary.cs
@@ -1,8 +1,13 @@
-﻿namespace MvcModels.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MvcModels.Models
 {
+    [Bind(nameof(City))]
     public class AddressSummary
     {
         public string City { get; set; }
+        [BindNever]
         public string Country { get; set; }
     }
 }
